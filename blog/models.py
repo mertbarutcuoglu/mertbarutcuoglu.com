cd88ebeaaacf287a18b0e668cad5218de589bb93
mertbarutcuoglu <kmbarutcuoglu19@my.sevkoleji.k12.tr>
2020-05-11 16:35:10 -0700

    added model for blog post

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
-
+STATUS = (
+    (0, "Draft"),
+    (1, "Ready to publish")
+)
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField()
     content = models.TextField()
-    publish_time = models.TimeField()
+    published_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        order_by = ['-published_on']
+        
     def __str__(self):
         return self.title
