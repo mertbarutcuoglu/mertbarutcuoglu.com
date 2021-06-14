b8285407c141a358017d2e62a764a45e04ddaf7a
mertbarutcuoglu <kbarutcu@students.cs.ubc.ca>
2021-06-13 23:24:44 -0700

    Updates requirements.txt and settings for the new domain

--- mertbarutcuoglu/settings.py
+++ mertbarutcuoglu/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = os.environ.get('DEBUG_VALUE', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'mertbarutcuoglu.tech']
+ALLOWED_HOSTS = ['127.0.0.1', 'mertbarutcuoglu.com']
 
 # Application definition
 
